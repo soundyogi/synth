4bccf6267493aa5efb17e4d088134b7cc81cf350
soundyogi@gmail.com <soundyogi@gmail.com>
2017-08-04 08:00:07 +0200

    added nexus js

--- synth.js
+++ synth.js
@@ -1,118 +1,137 @@
+"use strict";
 
-var grid = {};
-var audioCtx = new (window.AudioContext || window.webkitAudioContext)(); 
+/* GLOBALS (actually everything is global but meh)
+*/
 
-var frequencies = [];
-var MAJOR_SCALE = [0,2,4,5,7,9,11];
-var MINOR_SCALE = [0,2,3,5,7,8,10];
-var CHORD_PROGRESSION = [0, 4, 5, 3];
-var BASE_NOTE = 60;
-var BPM = 120;
-var currentTick = 0;
-var currentTab = 0;
+window.audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 
-function generateFrequencies()
+Nexus.context = audioCtx;
+
+/* CONSTANTS
+*/
+
+const MAJOR_SCALE = [0,2,4,5,7,9,11];
+const MINOR_SCALE = [0,2,3,5,7,8,10];
+const BASE_NOTE = 60; //??
+const BPM = 120;
+const FREQUENCIES = []
+
+// const means one time binding = cannot be re-assigned
+// let means can be re-assigned
+// var is deprecated
+
+/* STATE
+*/
+const STATE = {
+	chords_progression: [0, 4, 5, 3],
+  totalBeats: 1,
+	currentChordIndex: 0,
+	now: 0,
+	masterGain: audioCtx.createGain(),
+	osci: new Nexus.Oscilloscope('#oscilloscope',{ 'size': [300,150] })
+}
+
+function onBeat()
 {
-	for(var i = 0; i < 127; ++i)
-	{
-		a = Math.pow(2,1.0/12.0);
-		frequencies[i] = 440 * Math.pow(a, i - 81);
+	beat_indicator.classList.add("beat")
+	setTimeout(function(){ beat_indicator.classList.remove("beat") }, 100)
+
+	STATE.now = audioCtx.currentTime;
+
+	STATE.totalBeats++;
+
+	// teenage engineernig like chord looping
+	// if one bar is over change to next chord
+	// 4/4 takt
+  if(STATE.totalBeats % 4 === 0 ) {
+
+    if(STATE.currentChordIndex === STATE.chords_progression.length-1)
+		  STATE.currentChordIndex = 0
+    else
+		  (++STATE.currentChordIndex)
 	}
+
+}
+
+function onHalfBeat(){
+	sequencer.next();
+	half_beat_indicator.classList.add("beat")
+	setTimeout(function(){ half_beat_indicator.classList.remove("beat") }, 100)
 }
 
 function init()
 {
+	window.ui_init();
 	generateFrequencies();
-	for(var i = 0; i < WIDTH; ++i)
-	{
-		grid[i] = {};
-	}
-	for(var j = 0; j < HEIGHT; ++j)
-	{
-		for(var i = 0; i < WIDTH; ++i)
-		{
-			buttons[i][j].addEventListener("click",toggleGridButton);
-			grid[i][j] = {};
-			var g = grid[i][j];
-			g.active = false;
-		}
-	}
-	
+
+ 	STATE.osci.connect(STATE.masterGain);
+	STATE.masterGain.connect(audioCtx.destination)
 }
+init();
+// window.mainLoopId = setInterval(onTick, (60/(BPM*2))*1000);
+// stop loop with clearInterval("loopId")
+
+const BEAT =      new Nexus.Interval( 1000*60/BPM,   onBeat)
+const HALF_BEAT = new Nexus.Interval( 1000*60/BPM/2, onHalfBeat)
+
+BEAT.start()
+HALF_BEAT.start()
+
+// https://nexus-js.github.io/ui/api/#interval
+// more accurate cause of web audio clock
 
 
-function toggleGridButton(e)
+/* Sound and Instruments
+*/
+function Oscillator(options)
 {
-	var button = e.target;
-	var x = button.id.split("_")[1];
-	var y = button.id.split("_")[2];
-	var g = grid[x][y];
-	if(g.active)
+	return new OscillatorNode(audioCtx, options)
+}
+
+function PaulsInstrument(freq)
+{
+	STATE.now = audioCtx.currentTime;
+	var gain = audioCtx.createGain();
+	gain.gain.setValueAtTime(0, STATE.now);
+	gain.gain.linearRampToValueAtTime(0.333, STATE.now + 0.1);
+	gain.gain.linearRampToValueAtTime(0.0, STATE.now + 0.5);
+
+
+	gain.connect(STATE.masterGain);
+
+
+	var oscillator = Oscillator({type: "square", frequency: freq});
+	oscillator.connect(gain)
+	oscillator.start()
+	oscillator.stop(STATE.now + 0.5)
+
+	var oscillator2 = Oscillator({type: "square", frequency: freq+2});
+	oscillator2.connect(gain);
+	oscillator2.start();
+	oscillator2.stop(STATE.now + 0.5);
+
+	var oscillator3 = Oscillator({type: "square", frequency: freq-2});
+	oscillator3.connect(gain);
+	oscillator3.start();
+	oscillator3.stop(STATE.now + 0.5);
+
+
+}
+
+// generates all frequencie values, 440hz
+function generateFrequencies()
+{
+	for(var note = 0; note < 127; ++note)
 	{
-		g.active = false;
-		setGridButtonColor(x,y,"#243");
-	}
-	else
-	{
-		g.active = true;
-		setGridButtonColor(x,y,"#0FF");
+		const a = Math.pow(2,1.0/12.0); // comments pls
+		FREQUENCIES[note] = 440 * Math.pow(a, note - 81);
 	}
 }
 
+// get note based on scale and chord info
 function getNote(base, scale, chord, id)
 {
 	var scaleId = (7-id) + chord;
-	var note = base + scale[scaleId%7]+(Math.floor(scaleId/7)*12);
-	return frequencies[note];
+	var noteNumber = base + scale[scaleId%7]+(Math.floor(scaleId/7)*12);
+	return FREQUENCIES[noteNumber];
 }
-
-function onTick()
-{
-	++currentTick;
-	if(currentTick >= WIDTH)
-	{
-		currentTick = 0;
-		++currentTab;
-		if(currentTab >= 4)
-		{
-			currentTab = 0;
-		}
-	}
-	
-    var now = audioCtx.currentTime;
-	for(var j = 0; j < HEIGHT; ++j)
-	{
-		flashGridButton(currentTick,j,"#FFF", 200);
-		var g = grid[currentTick][j];
-		if(g.active)
-		{
-			var frequency = getNote(BASE_NOTE, MAJOR_SCALE, CHORD_PROGRESSION[currentTab], j);
-			var gain = audioCtx.createGain();
-			gain.connect(audioCtx.destination);
-			gain.gain.setValueAtTime(0, now);
-			gain.gain.linearRampToValueAtTime(0.333, now + 0.1);
-			gain.gain.linearRampToValueAtTime(0.0, now + 0.5);
-			var oscillator = audioCtx.createOscillator();
-			oscillator.type = "square";
-			oscillator.frequency.value = frequency;
-			oscillator.start();
-			oscillator.stop(now + 0.5);
-			oscillator.connect(gain);
-			var oscillator2 = audioCtx.createOscillator();
-			oscillator2.type = "square";
-			oscillator2.frequency.value = frequency+2;
-			oscillator2.start();
-			oscillator2.stop(now + 0.5);
-			oscillator2.connect(gain);
-			var oscillator3 = audioCtx.createOscillator();
-			oscillator3.type = "square";
-			oscillator3.frequency.value = frequency-2;
-			oscillator3.start();
-			oscillator3.stop(now + 0.5);
-			oscillator3.connect(gain);
-		}
-	}
-}
-
-init();
-var ticks = setInterval(onTick, (60/(BPM*2))*1000);
--- ui.js
+++ ui.js
@@ -1,59 +1,35 @@
 
-var WIDTH = 8;
-var HEIGHT = 8;
 
-var buttons = {};
+// I generated some nice pallette colors
+Nexus.colors.accent = "#9679EE"
+Nexus.colors.fill = "#243"
+Nexus.colors.dark = '#fff'
+Nexus.colors.light = "#fff"
+Nexus.colors.mediumDark = '#333'
+Nexus.colors.mediumLight = "#B5B7C6"
 
 
-function buildGrid(table)
-{
-	for(var i = 0; i < WIDTH; ++i)
-	{
-		buttons[i] = {};
-	}
-	
-	for(var j = 0; j < HEIGHT; ++j)
-	{
-		var tr = document.createElement('tr');
-		table.appendChild(tr);
-		for(var i = 0; i < WIDTH; ++i)
-		{
-			var th = document.createElement('th');
-			tr.appendChild(th);
-			var button = document.createElement('button');
-			button.id = "grid_" + i + "_" + j;
-			button.className = "grid_button";
-			button.active = false;
-			button.flash = false;
-			buttons[i][j] = button;
-			th.appendChild(button);
-		}
-	}
+function ui_init(){
+
+	sequencer = new Nexus.Sequencer('#sequencer',{
+	 'size': [700,350],
+	 'mode': 'toggle',
+	 'rows':    7,
+	 'columns': 16
+	})
+
+	STATE.sequencer = sequencer
+
+	sequencer.on('step',function(row) {
+	  console.log(row);
+
+    row.reverse().forEach(function( data,id ){
+      if(data){
+				var frequency = getNote(BASE_NOTE, MAJOR_SCALE, STATE.chords_progression[STATE.currentChordIndex], id);
+				PaulsInstrument(frequency)
+			}
+		})
+
+	})
+
 }
-
-function flashGridButton(x, y, color, duration = 200)
-{
-	var button = buttons[x][y];
-	var oldColor = button.style.backgroundColor;
-	button.flashTimeout = setTimeout(
-		function(button, color){
-			button.style.backgroundColor = color;
-			button.flashTimeout = null;
-		},
-		duration, button,oldColor);
-	button.style.backgroundColor = color;
-}
-
-function setGridButtonColor(x,y,color)
-{
-	var button = buttons[x][y];
-	if(button.flashTimeout != null)
-	{
-		clearTimeout(button.flashTimeout);
-	}
-	button.style.backgroundColor = color;
-}
-
-var table = document.getElementById("buttons");
-buildGrid(table);
-
